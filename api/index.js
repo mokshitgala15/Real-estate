a500c3c9158a22f890e572917c5b9f1eb2e165ca
Mokshit Gala <mokshitgala@gmail.com>
2024-09-08 13:05:34 -0700

    middleware and error handling

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("Connected to MongoDB");
-})
-.catch((err) =>{
-  console.log(err);
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.use(express.json());
@@ -21,3 +23,10 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({ sucess: false, statusCode, message });
+});
